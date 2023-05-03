ba8081869fdada973667f43de24a54772ab7a1dd
1Nurlan <nurlanabbasli@yahoo.com>
2023-05-03 13:51:14 +0400

    menim ilk repozitorim

---
+++ DemoProject/src/main/java/com/mycompany/demoproject/DemoProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demoproject;
+
+/**
+ *
+ * @author Monster
+ */
+public class DemoProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
